23f9795453015174c95349466eee5bd8d90d22bc
Marcelo Campos Silva <marcelo.home19@gmail.com>
2019-06-18 12:36:57 -0300

    Método Union com HashMaps Criado

--- Maps/src/main/java/pucrs/java/exercicio3/DesafioApp.java
+++ Maps/src/main/java/pucrs/java/exercicio3/DesafioApp.java
@@ -1,6 +1,8 @@
 package pucrs.java.exercicio3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DesafioApp {
 
@@ -20,6 +22,24 @@ public class DesafioApp {
 		long endTime = System.nanoTime();
 		
 		System.out.println("Tempo de execução ArrayList: " + (endTime - startTime));
+		System.out.println("-------------------------------");
+		
+		Map<Integer,Integer> listaMap = new HashMap<Integer,Integer>();
+		listaMap.put(1, 1);
+		listaMap.put(2, 2);
+		listaMap.put(3, 3);
+		
+		Map<Integer,Integer> listaMap2 = new HashMap<Integer,Integer>();
+		listaMap2.put(4, 4);
+		listaMap2.put(5, 5);
+		listaMap2.put(6, 6);
+		
+		long startTimeMap = System.nanoTime();
+		System.out.println("Método União");
+		System.out.println(unionMaps(listaMap,listaMap2));
+		long endTimeMap = System.nanoTime();
+		
+		System.out.println("Tempo de execução ArrayList: " + (endTimeMap - startTimeMap));
 	}
 
 	public static ArrayList<Integer> union(ArrayList<Integer> l1, ArrayList<Integer> l2){
@@ -37,5 +57,11 @@ public class DesafioApp {
 		}		
 		return resposta;
 	}
-	
+
+	public static Map<Integer, Integer> unionMaps(Map<Integer, Integer> l1, Map<Integer, Integer> l2){
+		Map<Integer, Integer> resposta = new HashMap<Integer, Integer>();
+		resposta.putAll(l1);
+		resposta.putAll(l2);
+		return resposta;
+	}
 }
